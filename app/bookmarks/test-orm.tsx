8e7315c6c3dd45b8b1eb69b65383bfc886bc4d96
Ulternae <ulternae@gmail.com>
2025-11-21 12:39:51 -0500

    Use state / effech request client 1 form

--- app/bookmarks/test-orm.tsx
+++ app/bookmarks/test-orm.tsx
@@ -1,23 +1,17 @@
 
-"use client"
+
 import { Heading, Text } from "@chakra-ui/react"
 
 import { Bookmark } from "@/components/bookmark"
-import { BookmarkType } from "./schema"
-import { useEffect, useState } from "react"
+import { orm } from "./db"
 
-export default function Bookmarks() {
-  const [bookmarks, setBookmarks] = useState<BookmarkType[]>([])
-
-  useEffect(() => {
-
-    fetch("http://localhost:3000/bookmarks/api")
-      .then((r) => r.json())
-      .then(({ data }) => {
-        setBookmarks(data)
-    })
-
-  }, [])
+export default async function Bookmarks() {
+  const data = await orm.query.bookmarks.findMany({
+    limit: 10,
+    with: {
+      author: true,
+    },
+  })
 
   return (
     <main className="mt-12">
@@ -31,7 +25,7 @@ export default function Bookmarks() {
       </header>
 
       <ul className="text-lg mt-10">
-        {bookmarks?.map((bookmark) => (
+        {data?.map((bookmark) => (
           <li className="border-b-2 py-4 px-6 my-2" key={bookmark.id}>
             <Bookmark {...bookmark} />
             {/* <div className="my-1 text-gray-600 text-xs ml-7">
